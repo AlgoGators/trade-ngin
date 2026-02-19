fcffb50661868ef66812e870f104dd9372f0f42e
Oliver Deng <oliver.t.deng@gmail.com>
2026-02-19 17:32:40 -0500

    initial backtest api structure

---
+++ include/trade_ngin/api/backtest_api.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "trade_ngin/backtest/backtest_types.hpp"
+
+using namespace trade_ngin::backtest;
+
+namespace trade_ngin::api {
+
+BacktestResults run_backtest();
+
+// Example usage in bt_portfolio.cpp
+// auto result = coordinator->run_portfolio(
+//     portfolio, config.strategy_config.symbols, config.strategy_config.start_date,
+//     config.strategy_config.end_date, config.strategy_config.asset_class,
+//     config.strategy_config.data_freq);
+
+}  // namespace trade_ngin::api
--- python/module.cpp
+++ python/module.cpp
@@ -6,8 +6,10 @@ PYBIND11_MODULE(trade_ngin, m) {
     m.doc() = "Python bindings for trade_ngin";
 
     bind_core_types(m);
+    bind_backtest_types(m);
     // bind_error_types(m);
-    // bind_strategy_types(m);
+    bind_strategy_types(m);
     bind_database(m);
     bind_base_strategy(m);
+    bind_backtest_api(m);
 }
