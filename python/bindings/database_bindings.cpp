6a39042f12a56fdad156995bf3291d4e20a0770c
Oliver Deng <oliver.t.deng@gmail.com>
2026-02-18 23:53:46 -0500

    fix: bind base strategy instead of strategy interface

---
+++ python/bindings/database_bindings.cpp
@@ -0,0 +1,14 @@
+#include <pybind11/pybind11.h>
+
+#include "trade_ngin/data/postgres_database.hpp"
+
+namespace py = pybind11;
+using namespace trade_ngin;
+
+void bind_database(py::module_& m) {
+    py::class_<trade_ngin::PostgresDatabase, std::shared_ptr<PostgresDatabase>>(m,
+                                                                                "PostgresDatabase")
+        .def(py::init<const std::string>())
+        .def("connect", &trade_ngin::PostgresDatabase::connect)
+        .def("disconnect", &trade_ngin::PostgresDatabase::disconnect);
+}
