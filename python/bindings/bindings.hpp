f834ddd205cfb0b5c2ffeba73547e7c670598f05
Oliver Deng <oliver.t.deng@gmail.com>
2026-02-27 01:57:17 -0500

    i'm going to lose my mind

--- python/bindings/bindings.hpp
+++ python/bindings/bindings.hpp
@@ -11,6 +11,11 @@ void bind_core_types(py::module_& m);
 void bind_backtest_types(py::module_& m);
 void bind_strategy_types(py::module_& m);
 // void bind_error_types(py::module_& m);
+
 void bind_database(py::module_& m);
+
 void bind_base_strategy(py::module_& m);
+void bind_trend_following_strategy(py::module_& m);
+void bind_trend_following_fast_strategy(py::module_& m);
+
 void bind_backtest_api(py::module_& m);
--- python/module.cpp
+++ python/module.cpp
@@ -9,7 +9,12 @@ PYBIND11_MODULE(trade_ngin, m) {
     bind_backtest_types(m);
     bind_strategy_types(m);
     // bind_error_types(m);
+
     bind_database(m);
+
     bind_base_strategy(m);
+    bind_trend_following_strategy(m);
+    bind_trend_following_fast_strategy(m);
+
     bind_backtest_api(m);
 }
