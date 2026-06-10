71620bac4f1fba2a31fcb15b0bbd651d8ed3961c
Oliver Deng <oliver.t.deng@gmail.com>
2026-06-09 22:50:56 -0400

    remove unnecessary bindings

--- python/bindings/bindings.hpp
+++ python/bindings/bindings.hpp
@@ -16,9 +16,5 @@ void bind_database(py::module_& m);
 void bind_instrument_registry(py::module_& m);
 
 void bind_base_strategy(py::module_& m);
-void bind_trend_following_config(py::module_& m);
-void bind_trend_following_strategy(py::module_& m);
-void bind_trend_following_fast_config(py::module_& m);
-void bind_trend_following_fast_strategy(py::module_& m);
 
 void bind_backtest_api(py::module_& m);
